4ec2ad03024ff51f470c17b5c35e9e9975b36f0b
Luciano Ramalho <luciano@ramalho.org>
2023-03-28 20:50:13 -0300

    revisão pré-LinuxTips

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp livro.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
+scp index.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
