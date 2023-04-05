c9446c2dda6d555d54fd99e95fccf3db4225c4b6
Luciano Ramalho <luciano@ramalho.org>
2023-04-05 20:15:20 -0300

    Novo script deploy.sh com rsync
    (ainda não consegui fazer a github action funcionar)

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
+
 #!/bin/bash
-scp index.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
+set -e  # exit when any command fails
+./build.sh
+rsync -avz --delete index.html dh_kqh7yy@pythonfluente.com:~/pythonfluente.com/
+
+#scp index.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
