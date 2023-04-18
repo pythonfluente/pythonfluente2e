5dcd046747326b60c07560ffd66dd22d2c46981a
Luciano Ramalho <luciano@ramalho.org>
2023-04-18 01:52:43 -0300

    melhoria no exemplo blogdom

--- capitulos/code/21-async/domains/asyncio/blogdom.py
+++ capitulos/code/21-async/domains/asyncio/blogdom.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
 import socket
-from keyword import kwlist
+from keyword import kwlist, softkwlist
 
 MAX_KEYWORD_LEN = 4  # <1>
+KEYWORDS = sorted(kwlist + softkwlist)
 
 
 async def probe(domain: str) -> tuple[str, bool]:  # <2>
@@ -16,7 +17,7 @@ async def probe(domain: str) -> tuple[str, bool]:  # <2>
 
 
 async def main() -> None:  # <5>
-    names = (kw for kw in kwlist if len(kw) <= MAX_KEYWORD_LEN)  # <6>
+    names = (kw for kw in KEYWORDS if len(kw) <= MAX_KEYWORD_LEN)  # <6>
     domains = (f'{name}.dev'.lower() for name in names)  # <7>
     coros = [probe(domain) for domain in domains]  # <8>
     for coro in asyncio.as_completed(coros):  # <9>
