075c51e13d22efd330b98622d281453a887ce0e3
Arturo Fonseca <arturofonsecas32@gmail.com>
2023-04-21 22:20:50 -0300

    torna carregamento de imagens "lazy" com regex
    Isto é feito utilizando o comando "sed" com regex no arquivo build.sh
    para adicionar a string 'loading="lazy"' nos atributos de cada elemento
    "img" no arquivo final HTML.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e  # exit when any command fails
 asciidoctor livro.adoc -o index.html
+sed -i -e 's/<img/& loading="lazy" /g' index.html # add lazy loading to images
 open index.html
