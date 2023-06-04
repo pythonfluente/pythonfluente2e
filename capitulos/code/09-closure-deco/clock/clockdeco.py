5691dc1b12253d32a0e38f128b7e28d7e0537124
Luciano Ramalho <luciano@ramalho.org>
2023-06-04 18:12:39 -0300

    ch07, ch09

---
+++ capitulos/code/09-closure-deco/clock/clockdeco.py
@@ -0,0 +1,17 @@
+import time
+import functools
+
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_lst = [repr(arg) for arg in args]
+        arg_lst.extend(f'{k}={v!r}' for k, v in kwargs.items())
+        arg_str = ', '.join(arg_lst)
+        print(f'[{elapsed:0.8f}s] {name}({arg_str}) -> {result!r}')
+        return result
+    return clocked
