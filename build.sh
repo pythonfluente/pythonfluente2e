c8e111668aceb84b3d94a8df0635062cc54b1f68
Luciano Ramalho <luciano@ramalho.org>
2023-09-28 00:13:50 -0300

    Prefacio, ainda om mojibake

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e  # exit when any command fails
-asciidoctor livro.adoc -o index.html
+asciidoctor Livro.adoc -o index.html
 open index.html
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 set -e  # exit when any command fails
-asciidoctor livro.adoc -o index.html
+asciidoctor Livro.adoc -o index.html
 
 #scp index.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
 rsync -avz --delete index.html dh_kqh7yy@pythonfluente.com:~/pythonfluente.com/
