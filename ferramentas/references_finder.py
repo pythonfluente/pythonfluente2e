a58fd10b194fd24e456775a2ae526bb2a3209051
Gabriel Almeida <gabriel.almeida075@gmail.com>
2025-02-19 21:18:17 -0300

    Fixing RegEx to match other pattners and ignore page break

--- ferramentas/references_finder.py
+++ ferramentas/references_finder.py
@@ -1,36 +1,20 @@
 # Isso é um rascunho
 import re
 
-book_dir = __file__[:-35]
-chapter_dir = "capitulos/"
-chapter1 = "cap01.adoc"
-
-
-F = open(book_dir+chapter_dir+chapter1,"r",encoding="utf-8")
-f = F.read()
-F.close()
-
-
-#com regex
-
+book_dir = __file__[:-32]
 
 list_of_files = [book_dir+"capitulos/cap%02d.adoc"%i for i in range(1,25)]
 
-
 def find_brackets(f):
-    brack_re = re.compile('\[\[[\w &]*\]\]')
+    brack_re = re.compile('\[(\[[\w-]+\])\]\n+(?:\[[\w=" -]+\]\n)?=* ?(.*)') 
     list_brack = re.findall(brack_re,f)
     return list_brack
 
-def find_brackets2(f):
-    brack_re = re.compile('(\[\[.*\]\])\n+=* ?(.*)')
-    list_brack = re.findall(brack_re,f)
-    return list_brack
 
-def find_markdown():
-    pass
-
-
+def find_LTRT(f):
+    ltrt_re = re.compile('<<([\w_]+)>>') 
+    list_ltrt = re.findall(ltrt_re,f)
+    return list_ltrt
 
 def find_all_brackets():
     dot_ini = ""
@@ -41,12 +25,12 @@ def find_all_brackets():
         F = open(files[i],"r",encoding="utf-8")
         f = F.read()
         F.close()
-        L = find_brackets2(f)  #Lista das Tuplas
-        dot_ini = dot_ini+"\n"+L[0][0][1:-1]+"\n"  # Remove a tupla do cap
+        L = find_brackets(f)  #Lista das Tuplas
+        dot_ini = dot_ini+"\n"+L[0][0]+"\n"  # Remove a tupla do título do cap
         L = L[1:]  # Tuplas do cap
         for i in L:
             j = list(i)
-            j[0]=j[0][2:-2]  # Remove os [[  ]]
+            j[0]=j[0][1:-1]  # Remove os [[  ]] antes de escrever no arquivo
             dot_ini = dot_ini+" = ".join(j)+"\n"
         
     out = open("chapter.ini","w",encoding="utf-8")
