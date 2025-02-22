369e5f4c762e89e96bcda9f201282c27e43f0bd6
Luciano Ramalho <luciano@ramalho.org>
2025-02-21 21:07:47 -0300

    melhorias na ferramenta

--- ferramentas/references_finder.py
+++ ferramentas/references_finder.py
@@ -1,43 +1,47 @@
-# Isso é um rascunho
+#!/usr/bin/env python3
+
 import re
 
-book_dir = __file__[:-32]
 
-list_of_files = [book_dir+"capitulos/cap%02d.adoc"%i for i in range(1,25)]
+book_dir = '../capitulos/'
 
-def find_brackets(f):
-    brack_re = re.compile('\[(\[[\w-]+\])\]\n+(?:\[[\w=" -]+\]\n)?=* ?(.*)') 
+
+list_of_files = [book_dir+"/cap%02d.adoc"%i for i in range(1,25)]
+
+
+def find_anchors(f):
+    brack_re = re.compile(r'\[(\[[\w-]+\])\]\n+(?:\[[\w=" -]+\]\n)?=* ?(.*)') 
     list_brack = re.findall(brack_re,f)
     return list_brack
 
 
-def find_LTRT(f):
-    ltrt_re = re.compile('<<([\w_]+)>>') 
+def find_xrefs(f):
+    ltrt_re = re.compile(r'<<([\w_]+)>>') 
     list_ltrt = re.findall(ltrt_re,f)
     return list_ltrt
 
-def find_all_brackets():
+
+def find_all_anchors():
     dot_ini = ""
-    brackets = []
+    anchors = []
     files = list_of_files
     
-    for i in range(23):
-        F = open(files[i],"r",encoding="utf-8")
-        f = F.read()
-        F.close()
-        L = find_brackets(f)  #Lista das Tuplas
-        dot_ini = dot_ini+"\n"+L[0][0]+"\n"  # Remove a tupla do título do cap
-        L = L[1:]  # Tuplas do cap
-        for i in L:
-            j = list(i)
-            j[0]=j[0][1:-1]  # Remove os [[  ]] antes de escrever no arquivo
-            dot_ini = dot_ini+" = ".join(j)+"\n"
+    for file_name in files:
+        with open(file_name,"r",encoding="utf-8") as fp:
+            f = fp.read()
+        anchors = find_anchors(f)  #Lista das Tuplas
+        dot_ini = dot_ini+"\n"+anchors[0][0]+"\n"  # insere o ident to capitulo
+        anchors = anchors[1:]  # Tuplas do cap
+        for anchor in anchors:
+            ident, text = anchor
+            ident = ident[1:-1]
+            dot_ini += f'{ident} = {text}\n'
         
-    out = open("chapter.ini","w",encoding="utf-8")
-    out.write(dot_ini)
-        
-        #brackets.extend()
-    return dot_ini
-        
-        
+    with open("anchors.ini","w",encoding="utf-8") as fp:
+        fp.write(dot_ini)
+
+
+if __name__ == "__main__":
+    find_all_anchors()
+    print("Done!")    
  
