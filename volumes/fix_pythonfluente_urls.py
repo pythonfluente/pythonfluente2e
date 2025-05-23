44892c1c3ec4e29cc72da8d4184d552df8fa9871
Luciano Ramalho <luciano@ramalho.org>
2025-05-23 16:13:26 -0300

    links do vol1 para o pythonfluente.com

--- volumes/fix_pythonfluente_urls.py
+++ volumes/fix_pythonfluente_urls.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 In the volume 1 *.adoc files, find links in this form:
 
@@ -19,4 +21,47 @@ Where {su} is the short URL for a URL to `pythonfluente.com`, such as:
 https://pythonfluente.com/2/#{ref}
 ```
 
+# Steps
+
+1. Edited chapters with those links to isolate the few relevant links into
+their own lines.
+
+2. Used `grep fpy.li/2p 1/*.adoc > linkspyfl-web.txt` to follect those links.
+
+3. Wrote list_pyfl_links(), use it to write `pyfl_refs.txt` 
+
+```
+./fix_pythonfluente_urls.py > pyfl_refs.txt
+```
+
+4. Sent `pyfl_refs.txt` to `short.py` from the
+[Fluent Python example code repo](https://github.com/fluentpython/example-code-2e/tree/cf996500070e0d712a3b088d29428f74ddc9fa1f/links) 
+
 """
+
+import re
+
+
+ADOC_LINK_RE = re.compile(r'(.*?)\[(.*)]')
+
+PYFL_BASE = 'https://pythonfluente.com/2/'
+
+def list_pyfl_links():
+    """Make list of links to PythonFluente.com"""
+    with open('links-pyfl-web.txt') as fp:
+        lines = fp.readlines()
+    
+    for line in lines:
+        chapter_path, link = line.split(':', maxsplit=1)
+        res = ADOC_LINK_RE.match(link)
+        if res is None:
+            print('NO MATCH:', line)
+            continue
+        else:
+            url, text = res.groups()
+            _, ref = url.split('?')
+            #print(ref, text, sep='\t')
+            print(PYFL_BASE + '#' + ref)
+
+if __name__ == '__main__':
+    list_pyfl_links()
