c8b611b98550d12c4b7a153c8ea70a272d5131a1
Luciano Ramalho <luciano@ramalho.org>
2025-05-23 12:09:56 -0300

    isolar links para pythonfluente.com no vol.1

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e  # exit when any command fails
 asciidoctor Livro.adoc -o index.html
 
-#scp index.html dh_kqh7yy@pendleton.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
-rsync -avz --delete index.html dh_kqh7yy@pythonfluente.com:~/pythonfluente.com/
+#scp index.html dh_kqh7yy@pdx1-shared-a1-19.dreamhost.com:/home/dh_kqh7yy/pythonfluente.com/index.html
+rsync -avz --delete index.html dh_kqh7yy@pythonfluente.com:~/pythonfluente.com/2/
 
-open https://pythonfluente.com
+open https://pythonfluente.com/2/
