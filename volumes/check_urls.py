96b82ffa0e9464cb1f9fa6542b0f784467a88479
Luciano Ramalho <luciano@ramalho.org>
2025-05-24 15:28:18 -0300

    verificando links (WIP)

--- volumes/check_urls.py
+++ volumes/check_urls.py
@@ -5,23 +5,31 @@ import asyncio
 import httpx
 import sys
 
-async def check_url(client: httpx.AsyncClient, start_url: str) -> tuple[str, int, str]:
+from typing import NamedTuple
+
+class StatusURL(NamedTuple):
+    status: int
+    url: str
+
+async def check_url(client: httpx.AsyncClient, start_url: str) -> tuple[int, str, list[str]]:
     req = client.build_request("GET", start_url)
     status = 0
-    end_url = '?'
+    responses = []
     while req is not None:
-        end_url = req.url
         try:
-            resp = await client.send(req)
-            status = resp.status_code
+            http_resp = await client.send(req)
+            response = StatusURL(http_resp.status_code, req.url)
         except httpx.HTTPError as e:
             print(e)
             print('URL:', req.url)
             status = -1
             break
-        req = resp.next_request
-
-    return end_url, status, start_url 
+        else:
+            responses.append(response)
+        req = http_resp.next_request
+    assert len(responses) > 0, 'NO RESPONSES: ' + start_url
+    final_url, *prior_urls = reversed(responses)
+    return status, final_url, prior_urls 
 
 
 async def main() -> None:
@@ -29,12 +37,9 @@ async def main() -> None:
     async with httpx.AsyncClient() as client:
         tasks = [check_url(client, url) for url in urls]
         async for task in asyncio.as_completed(tasks):
-            url, status, starting_url = await task
-            if url != starting_url:
-                print(status, url)
-                print('was', starting_url)
-            elif status != 200:
-                print(status, url)
+            status, final_url, prior_urls = await task
+            if prior_urls:
+                print(status, final_url, f'\n\t{prior_urls}')
 
 if __name__ == "__main__":
     asyncio.run(main())
