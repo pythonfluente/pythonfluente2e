e0abe391bc88e0c17b9a1a432d1ab6f6338ed99c
Luciano Ramalho <luciano@ramalho.org>
2025-05-26 09:01:57 -0300

    Update sort_check_output.py

--- volumes/sort_check_output.py
+++ volumes/sort_check_output.py
@@ -5,9 +5,9 @@ import re
 
 TASK_RE = re.compile(r'\[\s*(\d+)\]')
 
-def extract_key(line):
+def task_id(line):
     match = TASK_RE.search(line)
     return int(match.group(1)) if match else 0
 
-for line in sorted(fileinput.input(), key=extract_key):
+for line in sorted(fileinput.input(), key=task_id):
     print(line, end='')
