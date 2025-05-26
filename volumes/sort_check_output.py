75407190ff1d872fc96a8292ee0029b148202a34
Luciano Ramalho <luciano@ramalho.org>
2025-05-26 09:38:15 -0300

    Update sort_check_output.py

--- volumes/sort_check_output.py
+++ volumes/sort_check_output.py
@@ -6,8 +6,8 @@ import re
 TASK_RE = re.compile(r'\[\s*(\d+)\]')
 
 def task_id(line):
-    match = TASK_RE.search(line)
-    return int(match.group(1)) if match else 0
+    found = TASK_RE.match(line)
+    return int(match.group(1)) if found else 0
 
 for line in sorted(fileinput.input(), key=task_id):
     print(line, end='')
