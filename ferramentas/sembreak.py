9151505caed4e8bb4326c8fe53d78690acaef04b
Luciano Ramalho <luciano@ramalho.org>
2025-07-19 19:28:04 -0300

    quebras semanticas

---
+++ ferramentas/sembreak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import fileinput
+import re
+
+
+RE_PERIOD = re.compile(r'(\w\w)\. +([A-Z])')
+
+
+def sembreak(text):
+    return RE_PERIOD.sub(r'\1.\n\2', text)
+
+
+def main():
+    for line in fileinput.input(encoding="utf-8"):
+        if line[0] != '.':
+            line = sembreak(line)
+        print(line, end='')
+
+
+if __name__ == '__main__':
+    main()
