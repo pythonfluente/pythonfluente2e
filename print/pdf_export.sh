a4858858c7d48f58fd3e4c1e5067af9503e0b86e
Luciano Ramalho <luciano@ramalho.org>
2025-08-23 21:10:19 -0300

    vol1: formatação de avisos e tabelas

--- print/pdf_export.sh
+++ print/pdf_export.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-asciidoctor-pdf -a media=preprint --theme default-for-print $1
+set -e  # exit when any command fails
+asciidoctor-pdf -a media=preprint --theme pyfl-theme.yml $1
+open "${1%.*}.pdf"
