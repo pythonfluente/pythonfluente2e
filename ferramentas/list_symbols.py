63ea95389f88e155634eef46b4f255fe430b940e
Luciano Ramalho <luciano@ramalho.org>
2025-08-25 21:41:38 -0300

    ajustes print

--- ferramentas/list_symbols.py
+++ ferramentas/list_symbols.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import fileinput
-from collections import Counter
+from collections import Counter, namedtuple
 import unicodedata
+from operator import attrgetter
+
+UniChar = namedtuple('UniChar', 'char name categ count')
 
 def main():
     non_ascii = Counter()
@@ -13,10 +16,16 @@ def main():
             if not char.isascii():
                 non_ascii[char] += 1
 
-
-    for char, count in non_ascii.most_common():
+    chars = []
+    for char, count in non_ascii.items():
         name = unicodedata.name(char)
-        print(f'{count:5d}  {char}  {name}')
+        categ = unicodedata.category(char)
+        chars.append(UniChar(char, name, categ, count))
+       
+    chars.sort(key=attrgetter('categ', 'char'))
+        
+    for char, name, categ, count in chars:
+        print(f'U+{ord(char):04x}\t{char}\t{categ}\t{count}\t{name} ')
 
 
 if __name__ == '__main__':
