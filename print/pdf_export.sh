3cab484420e620480b2ac13186ee7cadaa6dc61d
Luciano Ramalho <luciano@ramalho.org>
2025-09-12 16:31:28 -0300

    fontes

--- print/pdf_export.sh
+++ print/pdf_export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e  # exit when any command fails
-bundle exec asciidoctor-pdf -v --theme pyfl-theme.yml $1
+bundle exec asciidoctor-pdf -v --theme pyfl-theme.yml -a pdf-fontsdir=./fonts/ $1
 open "${1%.*}.pdf"
