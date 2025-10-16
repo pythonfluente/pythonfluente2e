c8c86d765a66b570697d4d441cfb5ebdc9aa2ebf
Luciano Ramalho <luciano@ramalho.org>
2025-10-15 21:57:03 -0300

    cap12: wip

--- print/pdf_export.sh
+++ print/pdf_export.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e  # exit when any command fails
 name="${1%.*}-miolo.pdf"
-echo $name
 bundle exec asciidoctor-pdf -v --theme pyfl-fontmix-theme.yml -a pdf-fontsdir=./fonts/ $1
-pdfunite "${1%.*}.pdf" colofao.pdf $name
-open $name
+# pdfunite "${1%.*}.pdf" colofao.pdf $name
+# open $name
+echo "confira https://hexapdf.gettalong.org/"
+echo $name
+
