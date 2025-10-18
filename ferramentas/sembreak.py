a25fdd4801c7ba930beded70091bdd03a9735683
Luciano Ramalho <luciano@ramalho.org>
2025-10-18 19:09:04 -0300

    cap12: reformatar

--- ferramentas/sembreak.py
+++ ferramentas/sembreak.py
@@ -2,6 +2,9 @@
 
 import fileinput
 import re
+import shutil
+import sys
+from time import strftime
 
 
 RE_PERIOD = re.compile(r'(\w\w\w)\. +([A-Z])')
@@ -11,12 +14,19 @@ def sembreak(text):
     return RE_PERIOD.sub(r'\1.\n\2', text)
 
 
-def main():
-    for line in fileinput.input(encoding="utf-8"):
-        if line[0] not in '.| ':
-            line = sembreak(line)
-        print(line, end='')
+def main(adoc_name):
+    bkp_name = adoc_name + strftime('-%H-%M-%S') + '.bkp'
+    shutil.copy2(adoc_name, bkp_name)
+
+    with open(adoc_name) as fp:
+        lines = fp.readlines()
+
+    with open(adoc_name, 'wt') as fp:
+        for line in lines:
+            if line[0].isalpha():
+                line = sembreak(line)
+            fp.write(line)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
